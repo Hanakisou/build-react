24cae60e8ff69d8d11b8960d27cff127fd8161e9
王晖 <wb-wh227749@alibaba-inc.com>
2018-05-22 11:35:26 +0800

    chapter-2

---
+++ src/react-dom/render.js
@@ -0,0 +1,72 @@
+import { Componet } from '../react';
+import { setAttribute } from './dom';
+
+export function render(vnode, container){
+  container.appendChild(_render(vnode));
+}
+
+function _render(vnode){
+  if(vnode === undefined || vnode === null || typeof vnode === 'boolean') vnode = '';
+  if(typeof vnode === 'number') vnode = String(vnode);
+  if(typeof vnode === 'string'){
+    const textNode = document.createTextNode(vnode);
+    return textNode;
+  }
+  if(typeof vnode === 'function'){
+    const component = createComponent(vnode.tag, vnode.attrs);
+    setComponentProps(component, vnode.attrs);
+    return component.base;
+  }
+  const dom = document.createElement( vnode.tag );
+  if(vnode.attrs){
+    object.keys(vnode.attrs).forEach(key => {
+      const value = vnode.attrs[key];
+      setAttribute(dom, key, value)
+    })
+  }
+  vnode.children.forEach(child => render(child, dom));
+  return dom;
+}
+
+function createComponent(component, props){
+  let inst;
+  if(component.prototype && component.prototype.render){ // 如果是类定义组件，则直接返回实例
+    inst = new component( props );
+  }else{ // 如果是函数定义组件，则将其扩展为类定义组件
+    inst = new component( props );
+    inst.constructor = component;
+    inst.render = function() {
+      return this.constructor( props );
+    }
+  }
+  return inst;
+}
+
+function setComponentProps(component, props){
+  if(!component.base){
+    if(component.componentWillMount) component.componentWillMount();
+  }else if(component.componentWillReceiveProps){
+    component.componentWillReceiveProps();
+  }
+  component.props = props;
+  renderComponent(component);
+}
+
+export function renderComponent(component){
+  let base;
+  const renderer = component.render();
+  if(component.base && component.componentWillMount){
+    component.componentWillUpdate();
+  }
+  base = _render( renderer );
+  if(component.base){
+    if(component.componentDidUpdate) component.componentDidUpdate(); 
+  }else if(component.componentDidMount){
+    component.componentDidMount();
+  }
+  if(component.base && component.base.parentNode){
+    component.base.parentNode.replaceChild( base, component.base );
+  }
+  component.base = base;
+  base._component = component;
+}
---
+++ src/react/component.js
@@ -0,0 +1,16 @@
+import { renderComponent } from '../react-dom/render';
+
+class Component{
+  constructor( props = {} ){
+    this.isReactComponent = true;
+    this.state = {};
+    this.props = props;
+  }
+  setState(stateChange){
+    // 将修改合并到state
+    Object.assign(this.state, stateChange);
+    renderComponent(this);
+  }
+}
+
+export default Component;
